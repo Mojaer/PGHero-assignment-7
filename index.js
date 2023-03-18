8dddc9913abc52e778af99f6ebd6e7ad982504e8
Mojaer Ahmed <mojaermohi@gmail.com>
2023-03-18 14:43:34 +0600

    submit btn resolved

--- index.js
+++ index.js
@@ -74,8 +74,9 @@ const displayQuiz = (data) => {
   });
 };
 
-// EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
+
+//EventListener for quiz submit button
+document.querySelector("#submit").addEventListener("click", () => {
   if (answers.length < 6) {
     return;
   }
